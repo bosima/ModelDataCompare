72ad3678153fffafa895ac12bd12db489f2e0bef
bosima <bosima@126.com>
2015-06-25 21:13:10 +0800

    remove ghostDoc config file
    1、remove ghostDoc config file.
    2、modify copyright.

--- UnitTest/Properties/AssemblyInfo.cs
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FireflySoft.ModelDataCompare.UnitTest")]
-[assembly: AssemblyCopyright("Copyright ©  FireflySoft 2015")]
+[assembly: AssemblyCopyright("Copyright ©  FireflySoft.Net 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
